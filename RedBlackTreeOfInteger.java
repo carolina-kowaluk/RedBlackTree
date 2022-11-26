4a6e3a0f69bc4da60f4e507a18d1b0e78a88f759
Carolina Shypelenko Kowaluk <110862002+carolina-kowaluk@users.noreply.github.com>
2022-11-26 12:55:47 -0300

    adicionando arquivos

---
+++ RedBlackTreeOfInteger.java
@@ -0,0 +1,386 @@
+import java.util.NoSuchElementException;
+
+public class RedBlackTreeOfInteger {
+
+    private static final class Node {
+
+        public Node father;
+        public Node left;
+        public Node right;
+        public Integer element;
+        public boolean color; //vermelho:true, preto:false
+
+        public Node(Integer element, boolean color) {
+            this.element = element;
+            this.color = color;
+            father = nil;
+            left = nil;
+            right = nil;
+        }
+
+    }
+
+    // Atributos
+    private Node root;
+    private static Node nil;
+    private int count;
+
+    public RedBlackTreeOfInteger(){
+        count=0;
+        nil = new Node(0, false);
+        root = nil;
+    }
+
+    /**
+     * verifica se arvore esta vazia
+     * @return true se arvore esta vazia, senão retorna false
+     */
+    public boolean isEmpty(){
+        return (count == 0);
+    }
+
+    /**
+     * verifica quantos elementos tem na arvore
+     * @return numero de elementos na arvore
+     */
+    public int size(){
+        return count;
+    }
+
+    /**
+     * limpa o conteudo da arvore
+     */
+    public void clear(){
+        root = null;
+        count = 0;
+    }
+
+    /**
+     * verifica qual a altura da arvore
+     * @return a altura da arvore
+     */
+    public int height() {        
+        if (root == nil )
+            return 0;
+        else if (root.left==nil && root.right==nil)
+            return 0;
+        else
+            return height(root);
+    }    
+    private int height(Node n) {       
+        if ( n.left == nil && n.right == nil ) {
+            return 0;
+        }
+        else {
+            int h=0;
+            if ( n.left != nil )
+                h = Math.max(h, height(n.left));
+            if ( n.right != nil )
+                h = Math.max(h, height(n.right));
+            return 1+h;
+        }
+    } 
+
+    /**
+     * verifica se o element esta na arvore
+     * @param element
+     * @return true se element esta na arvore, senão retorna false
+     */
+    public boolean contains(Integer element) {
+        Node n = searchNodeRef(element, root);
+        return (n!=null);
+    }
+    private Node searchNodeRef(Integer element, Node target) {
+        if (element == null || target == null)
+            return null;
+        int c = element.compareTo(target.element);
+        if (c == 0)
+            return target;
+        if (c < 0)
+            return searchNodeRef(element, target.left);
+        else
+            return searchNodeRef(element, target.right);
+    }
+    
+    /**
+     * lista os elementos na arvore no caminhamento pre-fixado
+     * @return uma lista usando o caminhamento pre-fixado
+     */
+    public LinkedListOfInteger positionsPre() {
+        LinkedListOfInteger res = new LinkedListOfInteger();
+        positionsPreAux(root, res);
+        return res;
+    }
+    private void positionsPreAux(Node n, LinkedListOfInteger res) {
+        if (n != nil) {
+            res.add(n.element); //Visita o nodo
+            positionsPreAux(n.left, res); //Visita a subarvore da esquerda
+            positionsPreAux(n.right, res); //Visita a subarvore da direita
+        }
+
+    }
+
+    /**
+     * lista os elementos na arvore no caminhamento pos-fixado
+     * @return uma lista usando o caminhamento pos-fixado
+     */
+    public LinkedListOfInteger positionsPos() {
+        LinkedListOfInteger res = new LinkedListOfInteger();
+        positionsPosAux(root, res);
+        return res;
+    }
+    private void positionsPosAux(Node n, LinkedListOfInteger res) {
+        if (n != nil) {
+            positionsPosAux(n.left, res); //Visita a subarvore da esquerda
+            positionsPosAux(n.right, res); //Visita a subarvore da direita
+            res.add(n.element); //Visita o nodo
+        }
+    }
+
+    /**
+     * lista os elementos da arvore no caminhamento central
+     * @return uma lista usando o caminhamento central
+     */
+    public LinkedListOfInteger positionsCentral() {
+        LinkedListOfInteger res = new LinkedListOfInteger();
+        positionsCentralAux(root, res);
+        return res;
+    }
+    private void positionsCentralAux(Node n, LinkedListOfInteger res) {
+        if (n != nil) {
+            positionsCentralAux(n.left, res); //Visita a subarvore da esquerda
+            res.add(n.element); //Visita o nodo
+            positionsCentralAux(n.right, res); //Visita a subarvore da direita
+        }
+    }
+
+    /**
+     * lista os elementos da arvore no caminhamento em largura
+     * @return uma lista usando o caminhamento em largura
+     */
+    public LinkedListOfInteger positionsWidth() {
+        Queue<Node> fila = new Queue<>();
+        Node atual = null;
+        LinkedListOfInteger res = new LinkedListOfInteger();
+        if (root != nil) {
+            fila.enqueue(root);
+            while (!fila.isEmpty()) {
+                atual = fila.dequeue();
+                if (atual.left != nil) {
+                    fila.enqueue(atual.left);
+                }
+                if (atual.right != nil) {
+                    fila.enqueue(atual.right);
+                }
+                res.add(atual.element);
+            }
+        }
+        return res;
+    }  
+    
+    /**
+     * retorna o pai do elemento passado por parametro
+     * @param element
+     * @return o pai do element
+     */
+    public Integer getParent(Integer element){
+        Node aux = searchNodeRef(element, root);
+        if (aux==null) { //se nao encontrou element
+            throw new NoSuchElementException();
+        }
+        if (aux==root) { // se element esta na raiz
+            return null;
+        }
+        return aux.father.element; // retorna o elemento pai
+    }
+
+    /**
+     * retorna o avo do nodo passado por parametro
+     * @param n nodo
+     * @return o avo do nodo
+     */
+    public Node getAvo(Node n) {
+        if (n.father == null) {
+            return null; // Nao ter pai significa nao ter avo
+        }
+        Node pai = n.father;
+        return pai.father; // Se o pai tiver pai, retorna-o. Caso contrario, o 
+        // retorno do metodo getParent sera null, e o retorno será null.
+    }
+
+    /**
+     * retorna o tio do nodo passado por parametro
+     * @param n nodo
+     * @return o tio do nodo
+     */
+    public Node getTio(Node n) {
+        Node avo = getAvo(n);
+        if (avo == null) {
+            return null;
+        }
+        Node pai = n.father;
+        // Nao existe como saber se o pai eh maior ou menor que o avo, entao
+        // para recuperar o tio, comparamos os dois.
+        
+        if (pai.element.compareTo(avo.element) > 0) { // Se pai > avo
+            return avo.left; // retorna o filho da esquerda, que eh menor.
+            // Caso nao exista, retorna null.
+        } else { // Se pai < avo
+            return avo.right; // retorna o filho da direita, que eh maior.
+            // Caso nao exista, retorna null.
+        }   
+    }
+
+    /**
+     * faz rotação para esquerda
+     * @param n nodo
+     */
+    public void rotacaoEsquerda(Node n){
+       Node direito = n.right;
+       n.right = direito.left;
+       direito.left.father = n;
+       direito.left = n;
+       direito.father = n.father;
+       n.father = direito;
+
+       if(n != root){
+        if(direito.father.left == n){
+            direito.father.left = direito;
+        }
+        else
+            direito.father.right = direito;
+       }
+       else
+        root = direito;
+    }
+
+    /**
+     * faz rotação para direita
+     * @param n
+     */
+    public void rotacaoDireita(Node n){
+        Node esquerdo = n.left;
+        n.left = esquerdo.right;
+        esquerdo.right.father = n;
+        esquerdo.right = n;
+        esquerdo.father = n.father;
+        n.father = esquerdo;
+
+        if(n != root){
+            if(esquerdo.father.left == n){
+                esquerdo.father.left = esquerdo;
+            }
+            else
+                esquerdo.father.right = esquerdo;
+        }
+        else
+            root = esquerdo;
+    }
+
+    /**
+     * caso 1: o nodo esta na raiz da arvore
+     * @param n nodo
+     */
+    public void fixCase1(Node n) {
+        if (n.father == null) { // Se o no for raiz, pinta de preto.
+            //n.setColor(Color.BLACK);
+            n.color=false;
+        } else { // Se nao for, parte para o caso 2.
+            fixCase2(n);
+        }
+    }
+
+    /**
+     * caso 2: o pai do nodo é preto
+     * @param n nodo
+     */
+    public void fixCase2(Node n) {
+        //Color corDoPai = n.getParent().getColor();
+        if (n.father.color==true) { // Se o pai tiver cor vermelha, parte
+            fixCase3(n); // para o caso 3. Se nao, a arvore esta correta.
+        }
+    }
+
+    /**
+     * caso 3: o pai e tio são vermelhos e avo é preto, a cor de todos pode ser invertida
+     * @param n dodo
+     */
+    public void fixCase3(Node n) {
+        // So se chega a este metodo se o pai do no for vermelho.
+        // Verificaremos agora se o tio dele tambem eh vermelho.
+        Node tio = getTio(n); // Caso nao exista tio, a variavel recebe null.
+        if (tio != null && tio.color==true) {
+            // se existe tio e ele eh vermelho:
+            // pinta o pai e o tio de preto, o avo de vermelho e roda o fixcase1 no
+            // avo para fazer os ajustes, caso estes sejam necessarios.
+            Node pai = n.father;
+            pai.color = false;
+            tio.color = false;
+            
+            Node avo = getAvo(n);
+            avo.color = true;
+            fixCase1(avo);
+        } else {
+            fixCase4(n); // Caso nao haja tio ou ele seja preto, parte ao caso 4.
+        }
+    }
+
+    /**
+     * caso 4: o pai é vermelho mas o tio é preto 
+     *  - o nodo é filho a direita e seu pai é filho a esquerda
+     * @param n nodo
+     */
+    public void fixCase4(Node n) {
+        Node pai = n.father;
+        Node avo = getAvo(n).father;
+        
+        if (n.element.compareTo(pai.element) > 0 && pai.element.compareTo(avo.element) < 0) {
+            // Se o no eh filho da direita e o pai eh filho da esquerda
+            rotacaoEsquerda(pai);
+            
+            n = n.left; // o no passa a ser o seu filho da esquerda, que
+            // antes da rotacao era o seu pai.
+        } else if (n.element.compareTo(pai.element) < 0 && pai.element.compareTo(avo.element) > 0) {
+            // Se o no eh filho da esquerda e o pai eh filho da direita
+            rotacaoDireita(pai);
+            
+            n = n.right; // o no passa a ser o seu filho da direita, que
+            // antes da rotacao era o seu pai.
+        }
+        fixCase5(n);
+    }
+
+    /**
+     * caso 5: o pai é vermelho mas o tio é preto
+     * - o nodo é filho a esquerda e o pai é filho a esquerda
+     * @param n
+     */
+    public void fixCase5(Node n) {
+        Node avo = getAvo(n);
+        Node tio = getTio(n);
+        Node pai = n.father;
+        
+        // pinta o pai de preto e o avo de vermelho
+        avo.color = true;
+        pai.color = false;
+        
+        if (n.element.compareTo(pai.element) < 0 && pai.element.compareTo(avo.element) < 0) {
+            // se o no eh filho da esquerda e o pai tambem, 
+            // rotaciona o avo a direita.
+            rotacaoDireita(avo);
+        } else {
+            // se nao, quer dizer que o no eh filho da direita e o pai tambem,
+            // e rotacionamos o avo a esquerda.
+            rotacaoEsquerda(avo);
+        }
+    }
+
+    /**
+     * adiciona elemento na arvore
+     * @param element elemento a ser adicionado
+     * O(log n)
+     */
+    public void add(Integer element){
+
+    }
+}
